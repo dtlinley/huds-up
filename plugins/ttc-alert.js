a3ab60d0610dbeb3feffe64cadd405f21449acce
David Linley <dtlinley@gmail.com>
2016-10-09 22:21:24 -0400

    Implement TTC Alert plugin behaviour
    Basic TTC Alert functionality works. If there is an alert affecting the
    routes a user takes regularly (usually commute times) the user will be
    notified. There is currently no differentiation between important or
    unimportant routes, nor is there a consideration for multiple alerts
    affecting a user. Routes must also be entered with the correct spelling
    and punctuation, or they will not be matched.

--- plugins/ttc-alert.js
+++ plugins/ttc-alert.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const wreck = require('wreck');
+const cheerio = require('cheerio');
 const url = 'http://www.ttc.ca/Service_Advisories/all_service_alerts.jsp';
 
 exports.register = (server, options, next) => {
@@ -8,12 +9,28 @@ exports.register = (server, options, next) => {
     method: 'GET',
     path: '/plugins/ttc-alert',
     handler: (request, reply) => {
-      wreck.get(url, (err) => {
+      wreck.get(url, (err, response, payload) => {
         if (err) {
           return reply({ priority: 80, type: 'ttc-alert', data: { error: err } });
         }
 
-        const data = { priority: 0, type: 'ttc-alert', data: {} };
+        const dom = cheerio.load(payload.toString());
+        const alerts = [];
+        dom('.alert-content p.veh-replace').map((i, elem) => dom(elem).text())
+        .each((i, alert) => alerts.push(alert));
+        const routes = process.env.TTC_ROUTES_STATIONS.split(',').map(str => str.trim());
+        const filtered = alerts.filter(alert => {
+          let found = false;
+          routes.forEach(route => {
+            if (alert.includes(route)) {
+              found = true;
+            }
+          });
+          return found;
+        });
+
+        const priority = filtered.length > 0 ? 85 : 3;
+        const data = { priority, type: 'ttc-alert', data: { alerts: filtered } };
 
         return reply(data);
       });
--- test/plugins/ttc-alert.js
+++ test/plugins/ttc-alert.js
@@ -4,6 +4,8 @@ const expect = require('chai').expect;
 const Hapi = require('hapi');
 const proxyquire = require('proxyquire');
 const sinon = require('sinon');
+const fs = require('fs');
+const path = require('path');
 
 describe('ttcAlert Plugin', () => {
   let query;
@@ -11,13 +13,14 @@ describe('ttcAlert Plugin', () => {
   let wreck;
 
   beforeEach(done => {
+    const html = fs.readFileSync(path.join(__dirname, 'ttc-sample-data.html'), 'utf8');
     query = '/plugins/ttc-alert';
 
     server = new Hapi.Server();
     server.connection({});
 
     wreck = {
-      get: sinon.stub().yields(null, undefined, ''),
+      get: sinon.stub().yields(null, undefined, Buffer.from(html)),
     };
 
     server.start((err) => {
@@ -40,29 +43,84 @@ describe('ttcAlert Plugin', () => {
   });
 
   describe('#GET /plugins/ttc-alert', () => {
-    it('should return a priority 0 payload', done => {
-      server.inject(query).then(response => {
-        expect(JSON.parse(response.payload).priority).to.equal(0);
-        done();
+    describe('when there is an error loading the TTC data', () => {
+      beforeEach(() => {
+        wreck.get.yields({ status: 404, statusMessage: 'Not found' });
+      });
+
+      it('should respond with a high priority payload', done => {
+        server.inject(query).then(response => {
+          expect(JSON.parse(response.payload).priority).to.be.above(60);
+          done();
+        });
+      });
+
+      it('should send the network error back with the response', done => {
+        server.inject(query).then(response => {
+          expect(JSON.parse(response.payload).data.error.status).to.equal(404);
+          expect(JSON.parse(response.payload).data.error.statusMessage).to.equal('Not found');
+          done();
+        });
       });
     });
 
-    describe('when there is an error loading the TTC data', () => {
-      it('should respond with a high priority payload');
-
-      it('should send the network error back with the response');
-    });
-
     describe('when there are no alerts currently affecting the user', () => {
-      it('should respond with a low priority message');
+      let routes;
 
-      it('should respond with an empty array of alerts');
+      beforeEach(() => {
+        // Sample data has alerts for Line 4, Line 2 and Jane Station
+        routes = process.env.TTC_ROUTES_STATIONS;
+        process.env.TTC_ROUTES_STATIONS = 'Line 1, Yonge Station';
+      });
+
+      afterEach(() => {
+        process.env.TTC_ROUTES_STATIONS = routes;
+      });
+
+      it('should respond with a low priority message', done => {
+        server.inject(query).then(response => {
+          expect(JSON.parse(response.payload).priority).to.be.below(20);
+          done();
+        });
+      });
+
+      it('should respond with an empty array of alerts', done => {
+        server.inject(query).then(response => {
+          expect(JSON.parse(response.payload).data.alerts.length).to.equal(0);
+          done();
+        });
+      });
     });
 
     describe('when there is an alert affecting the user\'s route', () => {
-      it('should respond with a high priority message');
+      let routes;
 
-      it('should include the whole alert in the response');
+      beforeEach(() => {
+        // Sample data has alerts for Line 4, Line 2 and Jane Station
+        routes = process.env.TTC_ROUTES_STATIONS;
+        process.env.TTC_ROUTES_STATIONS = 'Line 4, Jane Station';
+      });
+
+      afterEach(() => {
+        process.env.TTC_ROUTES_STATIONS = routes;
+      });
+
+      it('should respond with a high priority message', done => {
+        server.inject(query).then(response => {
+          expect(JSON.parse(response.payload).priority).to.be.above(70);
+          done();
+        });
+      });
+
+      it('should include the whole alert in the response', done => {
+        server.inject(query).then(response => {
+          expect(JSON.parse(response.payload).data.alerts.length).to.equal(2);
+          expect(JSON.parse(response.payload).data.alerts[0]).to.equal(
+            'Elevator Alert: Jane Station, WB platform to concourse level, elevator is out of  service.' // eslint-disable-line max-len
+          );
+          done();
+        });
+      });
     });
   });
 });
